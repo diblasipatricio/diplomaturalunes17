18f90e038d39df1e9c2bdb476a5b63ecc428b0a8
Patricio diblasi <delentornoss@gmail.com>
2022-06-30 09:58:03 -0300

    Tarea M4U1 y M4U2 React

---
+++ M4U1_M4U2/frontend/src/pages/ContactoPage.js
@@ -0,0 +1,46 @@
+import "../styles/ContactoPage.css"
+const ContactoPage = (props) => {
+    return (
+        <main className="holder contacto">
+        <div>
+            <h2>Contacto Rápido</h2>
+            <form action="" method="" className="formulario">
+                <p>
+                    <label for="nombre">Nombre</label>
+                    <input type="text" name="" />
+                </p>
+                <p>
+                    <label for="email">Email</label>
+                    <input type="text" name="" />
+                </p>
+                <p>
+                    <label for="telefono">Teléfono</label>
+                    <input type="text" name="" />
+                </p>
+                <p>
+                    <label for="mensaje">Mensaje</label>
+                   <textarea name=""></textarea>
+                </p>
+                <p>
+                    <input type="submit" value="Enviar"/>
+                </p>
+            </form>
+        </div>
+
+        <div className="datos">
+            <h2>Otras vías de comunicación</h2>
+            <p>También puede contactarse con nosotros usando los siguientes medios:</p>
+                <ul>
+                    <li>Teléfono: 47263736</li>
+                    <li>Email: Delentornoss@gmail.com</li>
+                    <li>Facebook:</li>
+                    <li>Twitter:</li>
+                    <li>Skype:</li>
+                </ul>
+        </div>
+
+    </main>
+
+    );
+}
+export default ContactoPage;
